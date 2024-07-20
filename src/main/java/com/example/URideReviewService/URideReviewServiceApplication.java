d6287523bc5cb629d5f9b525d3ce80fd2dfa9ce7
Ujjwal Jain <ujjwaljain.u40@gmail.com>
2024-07-20 13:21:20 +0530

    basic spring data jpa config done

---
+++ src/main/java/com/example/URideReviewService/URideReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.URideReviewService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class URideReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(URideReviewServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/URideReviewService/URideReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.URideReviewService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class URideReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
