b0726dc58e76a27935cc5677f67f4d3aa8483256
Ujjwal Jain <ujjwaljain.u40@gmail.com>
2024-08-08 08:41:00 +0530

    Added db versioning using flyway

--- src/main/java/com/example/UberReviewService/models/Driver.java
+++ src/main/java/com/example/UberReviewService/models/Driver.java
@@ -25,6 +25,10 @@ public class Driver extends BaseModel{
     @Column(nullable = false, unique = true)
     private String licenseNumber;
 
+    private String phoneNumber;
+
+    private String address;
+
     @OneToMany(mappedBy = "driver",fetch = FetchType.LAZY)
     @Fetch(FetchMode.SUBSELECT)
     private List<Booking> bookings = new ArrayList<>();
